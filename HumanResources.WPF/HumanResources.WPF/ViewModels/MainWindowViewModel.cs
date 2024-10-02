b4299a5fd3b24f4f9433b3afd7fbe7e7478e4b53
fles <anotherfles@gmail.com>
2024-10-02 17:24:41 +0300

    Add mvvm folders to project

---
+++ HumanResources.WPF/HumanResources.WPF/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using FlesLib.WPF;
+using HumanResources.Client;
+using HumanResources.Client.Shared.Dto.Response;
+using System.Collections.ObjectModel;
+
+namespace HumanResources.WPF.ViewModels;
+
+public class MainWindowViewModel : ObservableObject
+{
+	private ObservableCollection<CompanyResponseDto> _companies = new ObservableCollection<CompanyResponseDto>();
+	private string _domain = "http://localhost:5000";
+	private HumanResourcesClient _humanResourcesClient;
+	public NotifyTaskCompletion<IEnumerable<CompanyResponseDto>> Companies { get; private set; }
+
+    public MainWindowViewModel()
+    {
+		_humanResourcesClient = new HumanResourcesClient(_domain);
+		Companies = new NotifyTaskCompletion<IEnumerable<CompanyResponseDto>>(GetCompanies());
+    }
+
+	public async Task<IEnumerable<CompanyResponseDto>> GetCompanies()
+	{
+		var companies = await _humanResourcesClient.Companies.GetAllAsync();
+		return companies;
+	}
+}
