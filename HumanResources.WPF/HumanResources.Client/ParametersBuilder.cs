eb4ebdd78186ba88c2b9ba59985f3e4ce2dc8370
fles <anotherfles@gmail.com>
2024-10-03 10:39:02 +0300

    Add Parameters builder class

---
+++ HumanResources.WPF/HumanResources.Client/ParametersBuilder.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+namespace HumanResources.Client;
+
+public class ParametersBuilder
+{
+	public string BuildParameters<T>(T parametersObj)
+	{
+		var objectProperties = parametersObj.GetType().GetProperties();
+		var propertyBuilder = new StringBuilder("?");
+
+		foreach (var property in objectProperties)
+		{
+			if (property.GetValue(parametersObj) != null)
+			{
+				propertyBuilder.Append($"{property.Name}={property.GetValue(parametersObj).ToString()}&");
+			}
+		}
+
+		propertyBuilder = propertyBuilder.Remove(propertyBuilder.Length, 1);
+
+		return propertyBuilder.ToString();
+	}
+}
