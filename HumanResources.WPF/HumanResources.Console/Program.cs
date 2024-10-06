7e365917314bc8b7fbfd9756cac416e7bd5a59c5
Ilya Katushenok <anotherfles@gmail.com>
2024-10-06 22:39:18 +0300

    Some tests in console

--- HumanResources.WPF/HumanResources.Console/Program.cs
+++ HumanResources.WPF/HumanResources.Console/Program.cs
@@ -13,18 +13,23 @@ var compResponse = await hrClient.Companies.GetAllAsync();
 Console.WriteLine(JsonSerializer.Serialize(compResponse, serializerOptions));
 
 var companyId = Guid.Parse(Console.ReadLine());
-var depResponse = await hrClient.Departmens.GetAllAsync(companyId);
-Console.WriteLine(JsonSerializer.Serialize(depResponse, serializerOptions));
+
+//var depResponse = await hrClient.Departmens.GetAllAsync(companyId);
+//Console.WriteLine(JsonSerializer.Serialize(depResponse, serializerOptions));
 
 var vacancies = await hrClient.Vacancies.GetAllAsync(companyId);
 Console.WriteLine(JsonSerializer.Serialize(vacancies, serializerOptions));
 
-var departmentId = Guid.Parse(Console.ReadLine());
-var workers = await hrClient.Workers.GetAllAsync(companyId, departmentId);
-Console.WriteLine(JsonSerializer.Serialize(workers, serializerOptions));
+//var departmentId = Guid.Parse(Console.ReadLine());
+//var workers = await hrClient.Workers.GetAllAsync(companyId, departmentId);
+//Console.WriteLine(JsonSerializer.Serialize(workers, serializerOptions));
 
-var professions = await hrClient.Professions.GetAllAsync();
-Console.WriteLine(JsonSerializer.Serialize(professions, serializerOptions));
+//var professions = await hrClient.Professions.GetAllAsync();
+//Console.WriteLine(JsonSerializer.Serialize(professions, serializerOptions));
+
+var vacId = Guid.Parse(Console.ReadLine());
+var prof = await hrClient.Vacancies.GetProfessionAsync(companyId, vacId);
+Console.WriteLine(JsonSerializer.Serialize(prof, serializerOptions));
 
 
 
