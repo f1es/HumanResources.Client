573d269c8f7be70d2fd428de4b24e9e8e79d8d10
fles <anotherfles@gmail.com>
2024-10-08 12:06:25 +0300

    Add view models to models

---
+++ HumanResources.WPF/HumanResources.WPF/ViewModels/Models/CompanyViewModel.cs
@@ -0,0 +1,19 @@
+﻿using FlesLib.WPF;
+using HumanResources.Client.Shared.Dto.Response;
+
+namespace HumanResources.WPF.ViewModels.Models;
+
+public class CompanyViewModel : ObservableObject
+{
+	private CompanyResponseDto _company = new CompanyResponseDto(Guid.NewGuid(), "Sample name", DateTime.Now);
+
+	public CompanyResponseDto Company
+	{
+		get => _company;
+		set
+		{
+			_company = value;
+			OnPropertyChanged();
+		}
+	}
+}
---
+++ HumanResources.WPF/HumanResources.WPF/ViewModels/Models/VacancyViewModel.cs
@@ -0,0 +1,19 @@
+﻿using FlesLib.WPF;
+using HumanResources.Client.Shared.Dto.Response;
+
+namespace HumanResources.WPF.ViewModels.Models;
+
+public class VacancyViewModel : ObservableObject
+{
+	private VacancyResponseDto _vacancy = new VacancyResponseDto(Guid.NewGuid(), DateTime.Now, "Sapmle description");
+
+	public VacancyResponseDto Vacancy
+	{
+		get => _vacancy;
+		set
+		{
+			_vacancy = value;
+			OnPropertyChanged();
+		}
+	}
+}
