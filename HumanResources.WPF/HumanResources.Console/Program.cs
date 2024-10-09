4d859f3188c50152c02681d83269fbba550b6bc2
Ilya Katushenok <anotherfles@gmail.com>
2024-10-09 22:57:52 +0300

    Console tests

--- HumanResources.WPF/HumanResources.Console/Program.cs
+++ HumanResources.WPF/HumanResources.Console/Program.cs
@@ -3,9 +3,23 @@ using HumanResources.Client.Shared.Parameters;
 using System.Text.Json;
 
 
-var domain = "http://localhost:5000";
+var domain = "https://localhost:44327";
 var hrClient = new HumanResourcesClient(domain);
 var serializerOptions = new JsonSerializerOptions()
 {
 	WriteIndented = true
 };
+
+try
+{
+
+	await hrClient.GetCredentialsToken();
+
+	var resp = await hrClient.Companies.GetAllAsync(new CompanyRequestParameters());
+	Console.WriteLine(JsonSerializer.Serialize(resp, serializerOptions));
+}
+catch (Exception ex)
+{
+	Console.WriteLine(ex.Message);
+}
+
