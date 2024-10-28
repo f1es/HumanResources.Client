78a624806093f78e44c0835f760f808e70ba1a3c
fles <anotherfles@gmail.com>
2024-10-28 18:55:21 +0300

    Add company mapper

---
+++ HumanResources.WPF/HumanResources.WPF/Mappers/CompanyMapper.cs
@@ -0,0 +1,30 @@
+﻿using HumanResources.Client;
+using HumanResources.Client.Shared.Dto.Response;
+using HumanResources.WPF.ViewModels.Models;
+using HumanResources.WPF.Views.Models;
+
+namespace HumanResources.WPF.Mappers;
+
+public static class CompanyMapper
+{
+	public static CompanyView ToView(this CompanyResponseDto company, HumanResourcesClient client)
+	{
+		var companyView = new CompanyView();
+		var dataContext = new CompanyViewModel(client);
+
+		dataContext.Company = company;
+		companyView.DataContext = dataContext;
+
+		return companyView;
+	}
+
+	public static IEnumerable<CompanyView> ToView(this IEnumerable<CompanyResponseDto> companies, HumanResourcesClient client)
+	{
+		var viewList = new List<CompanyView>();	
+
+		foreach(var company in companies)
+			viewList.Add(company.ToView(client));
+		
+		return viewList;
+	}
+}
