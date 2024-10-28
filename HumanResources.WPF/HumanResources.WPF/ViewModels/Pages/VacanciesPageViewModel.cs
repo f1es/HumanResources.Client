9e9e1d6ea01eb781ace234bca4b514ba4b9d1492
fles <anotherfles@gmail.com>
2024-10-28 19:05:09 +0300

    Add vacancies page view model

---
+++ HumanResources.WPF/HumanResources.WPF/ViewModels/Pages/VacanciesPageViewModel.cs
@@ -0,0 +1,18 @@
+﻿using FlesLib.WPF;
+using HumanResources.Client;
+using HumanResources.WPF.Views.Models;
+
+namespace HumanResources.WPF.ViewModels.Pages;
+
+public class VacanciesPageViewModel : ObservableObject
+{
+	private HumanResourcesClient _client;
+    private List<VacancyView> _vacancies = new List<VacancyView>();
+
+	public HumanResourcesClient Client => _client;
+
+    public VacanciesPageViewModel(HumanResourcesClient client)
+    {
+        _client = client;
+    }
+}
