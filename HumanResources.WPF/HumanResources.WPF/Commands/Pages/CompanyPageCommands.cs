c9a0c5f78fc41f0a9fec8985ea7ecdc95bb6df11
fles <anotherfles@gmail.com>
2024-10-28 19:02:31 +0300

    Add commands for company and professions

---
+++ HumanResources.WPF/HumanResources.WPF/Commands/Pages/CompanyPageCommands.cs
@@ -0,0 +1,48 @@
+﻿using FlesLib.WPF.Commands;
+using HumanResources.Client.Shared.Parameters;
+using HumanResources.WPF.Mappers;
+using HumanResources.WPF.ViewModels.Pages;
+using HumanResources.WPF.Views.Models;
+
+namespace HumanResources.WPF.Commands.Pages;
+
+public class CompanyPageCommands : CommandsDictionary
+{
+    public CompanyPageCommands(CompaniesPageViewModel viewModel)
+    {
+        var addCompanyCommand = new RelayCommand(c =>
+        {
+
+        });
+        AddCommand("Add", addCompanyCommand);
+
+        var searchCommand = new RelayCommand(async c =>
+        {
+            var parameters = new CompanyRequestParameters()
+            {
+                PageSize = 10,
+                PageNumber = viewModel.RequestParameters.PageNumber,
+                SearchTerm = viewModel.RequestParameters.SearchTerm,
+                OrederByQuery = viewModel.SortTypes[viewModel.SortType]
+            };
+            var companies = await viewModel.Client.Companies.GetAllAsync(parameters);
+
+            viewModel.Companies = new List<CompanyView>(companies.ToView(viewModel.Client));
+        });
+        AddCommand("Search", searchCommand);
+
+        var nextPageCommand = new RelayCommand(c =>
+        {
+            viewModel.PageNumber += 1;
+            searchCommand.Execute(null);
+        });
+        AddCommand("NextPage", nextPageCommand);
+
+        var previousPageCommand = new RelayCommand(c =>
+        {
+            viewModel.PageNumber -= 1;
+            searchCommand.Execute(null);
+        });
+        AddCommand("PreviousPage", previousPageCommand);
+    }
+}
---
+++ HumanResources.WPF/HumanResources.WPF/Commands/Pages/ProfessionPageCommands.cs
@@ -0,0 +1,37 @@
+﻿using FlesLib.WPF.Commands;
+using HumanResources.Client.Shared.Dto.Response;
+using HumanResources.Client.Shared.Parameters;
+using HumanResources.WPF.ViewModels.Pages;
+using HumanResources.WPF.Mappers;
+
+namespace HumanResources.WPF.Commands.Pages;
+
+public class ProfessionPageCommands : CommandsDictionary
+{
+    public ProfessionPageCommands(ProfessionsPageViewModel viewModel)
+    {
+        var searchCommand = new RelayCommand(async c =>
+        {
+            Dictionary<string, string> sortValues = new Dictionary<string, string>()
+            {
+                { "Name A-Z", $"{nameof(CompanyResponseDto.Name)} asc" },
+                { "Name Z-A", $"{nameof(CompanyResponseDto.Name)} desc" },
+            };
+
+            var parameters = new ProfessionRequestParameters()
+            {
+                PageSize = 10,
+                PageNumber = viewModel.RequestParameters.PageNumber,
+                SearchTerm = viewModel.RequestParameters.SearchTerm,
+                MaxSalary = viewModel.RequestParameters.MaxSalary,
+                MinSalary = viewModel.RequestParameters.MinSalary,
+                OrederByQuery = viewModel.SortTypes[viewModel.SortType],
+            };
+            var professions = await viewModel.Client.Professions.GetAllAsync(parameters);
+
+            viewModel.Professions = professions.ToView().ToList();
+        });
+        AddCommand("Search", searchCommand);
+
+    }
+}
