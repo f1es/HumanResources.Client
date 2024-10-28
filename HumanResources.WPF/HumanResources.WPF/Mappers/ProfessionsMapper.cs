97a90344c1275157399e4182ea9687b7e1a25669
fles <anotherfles@gmail.com>
2024-10-28 18:55:03 +0300

    Add professions mapper

---
+++ HumanResources.WPF/HumanResources.WPF/Mappers/ProfessionsMapper.cs
@@ -0,0 +1,27 @@
+﻿using HumanResources.Client.Shared.Dto.Response;
+using HumanResources.WPF.ViewModels.Models;
+using HumanResources.WPF.Views.Models;
+
+namespace HumanResources.WPF.Mappers;
+
+public static class ProfessionsMapper
+{
+	public static ProfessionView ToView(this ProfessionResponseDto profession)
+	{
+		var professionView = new ProfessionView();
+		var dataContext = new ProfessionViewModel();
+
+		dataContext.Profession = profession;
+		professionView.DataContext = dataContext;
+
+		return professionView;
+	}
+
+	public static IEnumerable<ProfessionView> ToView(this IEnumerable<ProfessionResponseDto> professions)
+	{
+		var professionsList = new List<ProfessionView>();
+		foreach(var profession in professions) 
+			professionsList.Add(profession.ToView());
+		return professionsList;
+	}
+}
