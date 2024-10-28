ce10dceb06bc900fa4bf8da43f5250088fb47c33
fles <anotherfles@gmail.com>
2024-10-28 19:02:49 +0300

    Add commands for company

---
+++ HumanResources.WPF/HumanResources.WPF/Commands/Models/CompanyModelCommands.cs
@@ -0,0 +1,19 @@
+﻿using FlesLib.WPF.Commands;
+using HumanResources.WPF.ViewModels.Models;
+
+namespace HumanResources.WPF.Commands.Models;
+
+public class CompanyModelCommands : CommandsDictionary
+{
+    public CompanyModelCommands(CompanyViewModel viewModel)
+    {
+        var deleteCommand = new RelayCommand(async c =>
+        {
+            await viewModel
+            .Client
+            .Companies
+            .DeleteAsync(viewModel.Company.Id);
+        });
+        AddCommand("Delete", deleteCommand);
+    }
+}
