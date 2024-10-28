b480bc85a2efb675c60daed1d336c4a1ab598124
fles <anotherfles@gmail.com>
2024-10-28 18:55:39 +0300

    add uint to string convertor

---
+++ HumanResources.WPF/HumanResources.WPF/Convertors/UintToStringConvertor.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace HumanResources.WPF.Convertors;
+
+public class UintToStringConvertor : IValueConverter
+{
+	public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		var uintValue = (uint)value;
+
+		if (uintValue == 0)
+			return string.Empty;
+
+		if (uintValue == uint.MaxValue) 
+			return string.Empty;
+
+		return value.ToString();
+	}
+
+	public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		if (uint.TryParse(value as string, out uint result))
+		{
+			return result;
+		}
+		return 0;
+	}
+}
