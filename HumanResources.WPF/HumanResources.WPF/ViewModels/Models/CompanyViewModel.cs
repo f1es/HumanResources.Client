4cb82030f51d69650f7e07da0b611b066ee6dc1e
fles <anotherfles@gmail.com>
2024-10-28 19:04:59 +0300

    Add company view model

--- HumanResources.WPF/HumanResources.WPF/ViewModels/Models/CompanyViewModel.cs
+++ HumanResources.WPF/HumanResources.WPF/ViewModels/Models/CompanyViewModel.cs
@@ -1,12 +1,15 @@
 ﻿using FlesLib.WPF;
+using HumanResources.Client;
 using HumanResources.Client.Shared.Dto.Response;
+using HumanResources.WPF.Commands.Models;
 
 namespace HumanResources.WPF.ViewModels.Models;
 
 public class CompanyViewModel : ObservableObject
 {
 	private CompanyResponseDto _company = new CompanyResponseDto(Guid.NewGuid(), "Sample name", DateTime.Now);
-
+	private HumanResourcesClient _humanResourcesClient;
+	public CompanyModelCommands Commands { get; set; }
 	public CompanyResponseDto Company
 	{
 		get => _company;
@@ -16,4 +19,14 @@ public class CompanyViewModel : ObservableObject
 			OnPropertyChanged();
 		}
 	}
+	public HumanResourcesClient Client => _humanResourcesClient;
+
+    public CompanyViewModel(HumanResourcesClient humanResourcesClient)
+    {
+        _humanResourcesClient = humanResourcesClient;
+		Commands = new CompanyModelCommands(this);
+    }
+
+    public CompanyViewModel()
+    { }
 }
